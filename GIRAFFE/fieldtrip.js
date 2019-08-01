e76ec5dbd1f5a24dd70d3c2f4a8523713d7bec10
Tim van Mourik <timvanmourik@gmail.com>
2019-08-01 12:59:48 +0200

    export module with async functions

--- GIRAFFE/fieldtrip.js
+++ GIRAFFE/fieldtrip.js
@@ -1,11 +1,13 @@
 module.exports = () => {
-    const LANGUAGE = 'Fieldtrip';
+    const LANGUAGE = 'fieldtrip';
 
-    const writeCode = () => "Fieldtrip test code"
-    const writeFiles = () => {
-        const fieldtripFilename = 'GIRAFFE/code/Dockerfile';
+    async function writeCode(nodes, links) {
+        return "Fieldtrip test code"
+    }
+    async function writeFiles(nodes, links) {
+        const fieldtripFilename = 'GIRAFFE/code/fieldtrip.m';
         return {
-            [fieldtripFilename]: await writeCode(nodes),
+            [fieldtripFilename]: await writeCode(nodes)
         }
     }
 
@@ -14,40 +16,3 @@ module.exports = () => {
         writeFiles,
     }
 }
-
-
-// const writePreamble = (nodes) => {
-//   const preamble = `% This is a Fieldtrip generator. Warning, here be dragons.`;
-//   return preamble;
-// };
-
-// const writeNodes = (nodes) => {
-//   const code = nodes && nodes.map((node) => itemToCode(node));
-//   return code && code.join('\r\n');
-// };
-
-// const itemToCode = (node) => {
-//   const codeArgument =
-//     node.code && node.code.find((a) => a.language === LANGUAGE);
-//   if (!codeArgument) {
-//     return '';
-//   }
-
-//   const code = ``;
-//   return code;
-// };
-
-// const writePostamble = () => {
-//   const code = '\r\n';
-//   return code;
-// };
-
-// export function writeCode(nodes, links) {
-//   const preamble = writePreamble(nodes);
-//   const nodeCode = writeNodes(nodes);
-//   // const parameters = writeParameters();
-//   const postAmble = writePostamble();
-
-//   // return [preamble, nodeCode, linkCode, postAmble].join("\r\n");
-//   return [preamble, nodeCode, postAmble].join('\r\n');
-// }
