92d8c175fa8b88eacaa4ad3bb1ad04ecde35afd0
Kristijan Armeni <kriarm@dccn.nl>
2019-08-07 15:33:59 +0200

    added code to parse function definitions and write output arguments to code.argout field

--- FieldTrip2GiraffeTools.m
+++ FieldTrip2GiraffeTools.m
@@ -23,34 +23,62 @@ for j = 1:length(filenames)
     f = fopen(file);
     numberOfPorts = 0;
     ports = [];
+    
+    lineCount = 0;
+    
+    code = [];
     while true
+        
+        % write parsed code elements in this var
+        code.language = categoryName;
+        
+        lineCount = lineCount + 1;
         
         line = fgetl(f);
         if line == -1
             break
         end
         
+        % check if there are output arguments in function definition (= line 1)
+        if lineCount == 1 && ~isempty(regexp(line, 'function.*=', 'once'))
+        
+            % look for any text between 'function' and '=' sign
+            outputArg = regexp(line, '(?<=function.)(.*)(?=.=)', 'match');
+            if ~isempty(outputArg)
+                hasOutput = true;
+                %outputArg{1}(regexp(outputArg{1}, '[\[,\]]')) = []; % remove '[' and ']'
+            end
+        
+            code.argout.exist = hasOutput;
+            code.argout.name = outputArg;
+            
+        end
+        
         if strfind(line, '%') ~= 1
             continue;
         end
         if strfind(line, '%   cfg.') == 1
+            
              numberOfPorts = numberOfPorts + 1;
              ports(numberOfPorts).input = true;
              ports(numberOfPorts).output = true;
              ports(numberOfPorts).visible = true;
              ports(numberOfPorts).editable = true;
             
+             % parse the parameter option string
              template = '%%  cfg.%s = %s';
              parsed = textscan(line, template);
              [parameter, comment] = deal(parsed{:});
              ports(numberOfPorts).name = parameter{1};
             
-             code = [];
-             code.language = categoryName;
-             code.argument.name = line(5:end);
+             code.cfgfields.name = line(5:end);
              ports(numberOfPorts).code = {code};
         end
+        
+        
     end
+    % fprintf('\n%d Found %s output argument for %s', lineCount, outputArg{1}, filenames(j).name(1:end - 2))
+
     fclose(f);
     node.toolbox = categoryName;
     node.category = {categoryName};
@@ -68,15 +96,8 @@ ft.name = categoryName;
 ft.nodes = nodes;
 
 %%
-f = fopen(fullfile(saveLocation, 'fieldtrip.JSON'), 'w');
+f = fopen(fullfile(saveLocation, 'fieldtrip-test.JSON'), 'w');
 options.ParseLogical = true;
 % options.Compact = true;
 fwrite(f, savejson('toolboxes', {ft}, options));
 fclose(f);
-
-
-
-
-
-
-
